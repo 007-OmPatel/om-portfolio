31faa2352e8cf0700aed578797d856c57f8086ad
007-OmPatel <141636225+007-OmPatel@users.noreply.github.com>
2025-04-15 20:13:26 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+
+document.getElementById("root").innerHTML = `<div class='p-10 text-xl'>Hi, this is Om Patel's portfolio.<br>To see the full version, please run the React build using npm or deploy with Vite.</div>`;
