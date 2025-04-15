527804d0a72e512bc55a58d0553e924b0a85d47b
007-OmPatel <141636225+007-OmPatel@users.noreply.github.com>
2025-04-15 20:23:48 +0100

    Delete app.js

--- app.js
+++
@@ -1,2 +0,0 @@
-
-document.getElementById("root").innerHTML = `<div class='p-10 text-xl'>Hi, this is Om Patel's portfolio.<br>To see the full version, please run the React build using npm or deploy with Vite.</div>`;
